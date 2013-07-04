acfea7c7162edea263c1628a4ad3dfd8d3cc6b6d
Budry <zarubaondra@gmail.com>
2013-07-04 15:30:58 +0200

    Added janmarek/mockista package

---
+++ Kappa/Tester/MockTestCase.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the Kappa\Tester package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Tester;
+use Mockista\Registry;
+
+/**
+ * Class TestCase
+ * @package Kappa\Tester
+ */
+abstract class MockTestCase extends TestCase
+{
+	/** @var \Mockista\Registry */
+	protected $mockista;
+
+	protected function setUp()
+	{
+		$this->mockista = new Registry();
+	}
+
+	protected function tearDown()
+	{
+		$this->mockista->assertExpectations();
+	}
+}
