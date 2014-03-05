fb41559d98c52afa90c1b70adfa7bad28a7369b1
Budry <zarubaondra@gmail.com>
2014-03-05 11:20:31 +0100

    Fixed variable name

--- src/Kappa/Tester/Helpers.php
+++ src/Kappa/Tester/Helpers.php
@@ -18,21 +18,21 @@ class Helpers
 {
 	/**
 	 * @param string|null $prefix
-	 * @param string|null $ext
+	 * @param string|null $sufix
 	 * @return string
 	 * @throws InvalidArgumentException
 	 */
-	public static function getRandName($prefix = null, $ext = null)
+	public static function getRandName($prefix = null, $sufix = null)
 	{
 		if ($prefix !== null && !is_string($prefix)) {
 			throw new InvalidArgumentException(__METHOD__ . ": Prefix must be string");
 		}
-		if ($ext !== null && !is_string($ext)) {
+		if ($sufix !== null && !is_string($sufix)) {
 			throw new InvalidArgumentException(__METHOD__ . ": Sufix must be string");
 		}
 		$string = $prefix;
 		$string .= rand(1000000, 9999999) . time();
-		$string .= $ext;
+		$string .= $sufix;
 
 		return $string;
 	}
