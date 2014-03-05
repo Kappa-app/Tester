bdd5fa1341bee6a87d9f635de29375128105d804
Budry <zarubaondra@gmail.com>
2014-03-05 11:28:46 +0100

    Typo

--- tests/Kappa/bootstrap.php
+++ tests/Kappa/bootstrap.php
@@ -6,8 +6,6 @@
  *
  * For the full copyright and license information, please view the license.md
  * file that was distributed with this source code.
- *
- * @testCase
  */
 
 if (@!include __DIR__ . '/../../vendor/autoload.php') {
